e4b43af094398679cb09f509fce50baeffda5d37
SEPIN <DELL@DESKTOP-SCUEB03>
2020-06-17 22:55:49 +0700

    bikin 3pola besar

--- TTS-GRAFKOM-DOSEN/Source.cpp
+++ TTS-GRAFKOM-DOSEN/Source.cpp
@@ -5,45 +5,77 @@ void display() {
 	glClear(GL_COLOR_BUFFER_BIT);
 	glColor3f(1.0, 0.0, 0.0);
 
-	glBegin(GL_POLYGON);//pola besar
+	glBegin(GL_POLYGON);//pola besar 1
 	glColor3ub(205, 92, 92);
-	glVertex2f(-45, -10);
-	glVertex2f(0, 35);
-	glVertex2f(45, -10);
-	glVertex2f(0, -55);
+	glVertex2f(-100, 0);
+	glVertex2f(0, 145);
+	glVertex2f(100, 0);
+	glVertex2f(0, -145);
 	glEnd();
 
-	glBegin(GL_POLYGON);//pola itam kecil atas
-	glColor3f(0.0, 0.0, 0.0);
-	glVertex2f(-10, 0);
-	glVertex2f(0, 10);
-	glVertex2f(10, 0);
-	glVertex2f(0, -10);
+	glBegin(GL_POLYGON);//pola besar 2
+	glColor3ub(205, 92, 92);
+	glVertex2f(0, 150);
+	glVertex2f(-100, 295);
+	glVertex2f(0, 440);
+	glVertex2f(100, 295);
 	glEnd();
 
-	glBegin(GL_POLYGON); // pola itam kecil kanan 
-	glColor3f(0.0, 0.0, 0.0);
-	glVertex2f(5, -10);
-	glVertex2f(15, -20);
-	glVertex2f(25, -10);
-	glVertex2f(15, 0);
+	glBegin(GL_POLYGON);//pola besar 3
+	glColor3ub(205, 92, 92);
+	glVertex2f(0, -150);
+	glVertex2f(-100, -295);
+	glVertex2f(0, -440);
+	glVertex2f(100, -295);
 	glEnd();
 
-	glBegin(GL_POLYGON); // pola itam kecil kiri 
-	glColor3f(0.0, 0.0, 0.0);
-	glVertex2f(-5, -10);
-	glVertex2f(-15, -20);
-	glVertex2f(-25, -10);
-	glVertex2f(-15, 0);
-	glEnd();
+	//glBegin(GL_POLYGON);//pola besar 
+	//glColor3ub(205, 92, 92);
+	//glVertex2f(120, -290);
+	//glVertex2f(-100, 295);
+	//glVertex2f(0, 440);
+	//glVertex2f(100, 295);
+	//glEnd();
 
-	glBegin(GL_POLYGON); //pola itam kecil bawah 
-	glColor3f(0.0, 0.0, 0.0);
-	glVertex2f(0, -12);
-	glVertex2f(10, -22);
-	glVertex2f(0, -32);
-	glVertex2f(-10, -22);
-	glEnd();
+	//glBegin(GL_POLYGON);//pola besar
+	//glColor3ub(205, 92, 92);
+	//glVertex2f(120, 20);
+	//glVertex2f(20, 165);
+	//glVertex2f(120, 330);
+	//glVertex2f(220, 165);
+	//glEnd();
+
+	//glBegin(GL_POLYGON);//pola itam kecil atas
+	//glColor3f(0.0, 0.0, 0.0);
+	//glVertex2f(-10, 0);
+	//glVertex2f(0, 10);
+	//glVertex2f(10, 0);
+	//glVertex2f(0, -10);
+	//glEnd();
+
+	//glBegin(GL_POLYGON); // pola itam kecil kanan 
+	//glColor3f(0.0, 0.0, 0.0);
+	//glVertex2f(5, -10);
+	//glVertex2f(15, -20);
+	//glVertex2f(25, -10);
+	//glVertex2f(15, 0);
+	//glEnd();
+
+	//glBegin(GL_POLYGON); // pola itam kecil kiri 
+	//glColor3f(0.0, 0.0, 0.0);
+	//glVertex2f(-5, -10);
+	//glVertex2f(-15, -20);
+	//glVertex2f(-25, -10);
+	//glVertex2f(-15, 0);
+	//glEnd();
+
+	//glBegin(GL_POLYGON); //pola itam kecil bawah 
+	//glColor3f(0.0, 0.0, 0.0);
+	//glVertex2f(0, -12);
+	//glVertex2f(10, -22);
+	//glVertex2f(0, -32);
+	//glVertex2f(-10, -22);
+	//glEnd();
 	glFlush();
 }
 
